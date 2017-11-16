aef43cfa859d1e5d45a46adb730eeacec793514c
David Barrera <davsanba@gmail.com>
2017-11-16 00:36:10 -0500

    Variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,25 +23,21 @@ class ApplicationController < ActionController::API
 
 
   def ms_ip(ms)
-    host = "http://192.168.99.101:"
-    hostr = "http://192.168.99.102:"
-    hosts = "http://192.168.99.103:"
-    host2 = "http://168.176.38.67:"
     case ms
     when "sm" #send mail
-      host2 += "3034"
+      ENV["SANTIAGO"] += "3034"
     when "in" #Inbox
-      host2 += "3032"
+      ENV["SANTIAGO"] += "3032"
     when "nt" #Notifications
-      host2 += "3033"
+      ENV["SANTIAGO"] += "3033"
     when "rg" #Register
-      hostr += "3034"
+      ENV["REG"] += "3034"
     when "ss" #Sessions
-      hosts += "3035"
+      ENV["SESS"] += "3035"
     when "schs" # programacion
-      host += "3006"
+      ENV["PROG"] += "3006"
     when "ldap" # programacion
-      host += "4001"
+      ENV["LDAP"] += "4001"
     end
     return host
   end
