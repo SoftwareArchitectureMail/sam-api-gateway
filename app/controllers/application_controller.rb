5159b4aa413d051b9e2d64fe38693703acb62a51
Anderson Kavir Osorio <akosoriod@unal.edu.co>
2017-11-16 03:29:26 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,21 +23,22 @@ class ApplicationController < ActionController::API
 
 
   def ms_ip(ms)
+    host = ""
     case ms
     when "sm" #send mail
-      ENV["SANTIAGO"] += "3034"
+      host += ENV["SANTIAGO"] + "3034"
     when "in" #Inbox
-      ENV["SANTIAGO"] += "3032"
+      host += ENV["SANTIAGO"] + "3032"
     when "nt" #Notifications
-      ENV["SANTIAGO"] += "3033"
+      host += ENV["SANTIAGO"] + "3033"
     when "rg" #Register
-      ENV["REG"] += "3034"
+      host += ENV["REG"] + "3034"
     when "ss" #Sessions
-      ENV["SESS"] += "3035"
+      host += ENV["SESS"] + "3035"
     when "schs" # programacion
-      ENV["PROG"] += "3006"
+      host += ENV["PROG"] + "3006"
     when "ldap" # programacion
-      ENV["LDAP"] += "4001"
+      host += ENV["LDAP"] + "4001"
     end
     return host
   end
